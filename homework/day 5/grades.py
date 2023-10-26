ff3e6f8815c9f5bd4b6bc50caa0b1f15549a12b9
mariamwereteli <145152664+mariamwereteli@users.noreply.github.com>
2023-10-26 22:07:09 +0400

    Add files via upload

---
+++ homework/day 5/grades.py
@@ -0,0 +1,18 @@
+num_grades=int(input("6"))
+userscore1=int(input("enter score N1"))
+subject=input(english)
+userscore2=int(input("enter score N2"))
+subject=input(Georgian)
+userscore3=int(input("eenter score N3"))
+subject=(Chemistry)
+userscore4=int(input("enter score N4"))
+subject=(history)
+userscore5=int(input("enter score N5"))
+avg_score=(userscore1 + userscore2 + userscore3 + userscore4 + userscore5) / 4
+if avg_score>=9 and avg_score<=10
+print(გილოცავ მატრიცელო შენი ქულაა 10 და მოიგე ბანძი ტოსტერი რომლსაც მთელი ცხოვრება შეალიე)
+if avg_score>=1 and avg_score<=5
+print(გილოცავ, შენ გაექეცი მატრიცას)
+if avg_score>=6 and avg_score<=8
+print(you mid)
+
---
+++ homework/day 5/homework.py
@@ -0,0 +1,10 @@
+
+userscore1=int(input("enter score N1"))
+avg_score=(userscore1) / 1
+if avg_score>=10000 and avg_score<=1000000
+print(GOA-ში სწავლობდი?)
+if avg_score>=1000 and avg_score<=10000
+print(you mid)
+if avg_score=>1 and avg_score<=1000
+print(შმოსულიყავი GOA-ში, მატრიცელო)
+
