548741941797785abd2f23d7de12948902be4b55
mariamwereteli <145152664+mariamwereteli@users.noreply.github.com>
2023-10-29 22:49:42 +0400

    Add files via upload

---
+++ homework/day 9/homework.py
@@ -0,0 +1,3 @@
+nums = [2, 4, 6, 3, 4, 7, 2, 9,]
+nums.append(2, 4,)
+print(nums)
---
+++ homework/day 9/homework2.py
@@ -0,0 +1,8 @@
+family = ["Asmati", "Pavle", "Mariami"]
+full_sentence = "My moms name is: {}, My dads name is: {}, My name is: {}".format(family[0],family[1],family[2])
+print(full_sentence)
+age = ["46", "48", "13"]
+full_sentence = "My mom is: {}, My dad is: {}, I'am: {}".format(family[0], family[1], family[2])
+print(full_sentence)
+age2 = ["56", "58", "23"]
+full_sentence = "In 10 years my mom will be {}, In 10 years my dad will be {}, In 10 years I will be {}".format(family[0], family[1], family[2])
